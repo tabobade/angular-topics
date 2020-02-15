d068898e953d08db6ad4a49af056ef80372a6e9b
tabobade <tanveer.bobade@gmail.com>
2020-02-15 12:20:48 +0530

    checkbox code added

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 import { DataService } from '../data.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { from } from 'rxjs';
@@ -10,7 +10,7 @@ import { User } from '../user';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
+hobbiesArr:string[]=["CRICKET","MUSIC","TRAVELLING","COOKINGs"];
 
   userForm: FormGroup;
 
@@ -20,7 +20,8 @@ export class RegistrationComponent implements OnInit {
     this.userForm = this.formBuilder.group({
       fname: ['', Validators.required],
       lname: [''],
-      gender: []
+      gender: [''],
+hobbies:new FormArray( [],[Validators.required])
     });
 
 
@@ -42,15 +43,42 @@ if(fname!=null)
     gender:user.gender 
 
   });
+  var array :FormArray=this.userForm.get("hobbies") as FormArray
+  user.hobbies.forEach(element=>{
+    array.push(new FormControl(element));
   
-  }
+  })
+}
   }
 
 
   submitDetails() {
     console.log(this.userForm.value);
-    this.dataService.saveData(this.userForm.value);
+   this.dataService.saveData(this.userForm.value);
     this.router.navigateByUrl("view.htm");
   }
+
+
+  onChangeHobbies(event)
+
+  
+      {   var array: FormArray = this.userForm.get("hobbies") as FormArray;
+
+      if (event.target.checked) {
+      array.push(new FormControl(event.target.value));
+      }
+      else {
+        var index = 0;
+        this.userForm.get("hobbies").value.forEach(element => {
+          if (element == event.target.value) {
+            array.removeAt(index);
+        }
+        index++;
+      });
+    
+    }
+  
+  }
+
 
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -3,4 +3,5 @@ export class User {
     fname:string;
     lname:string;
     gender:string;
+    hobbies:string[];
 }
